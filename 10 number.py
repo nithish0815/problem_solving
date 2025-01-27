e32b3adbf682b6cfbdefb2f4cc0cc39609ae5b70
nithishkathirvel <kathirvelnithish08@gmail.com>
2025-01-27 21:58:28 +0530

    for loop practice

---
+++ 10 number.py
@@ -0,0 +1,10 @@
+n=int(input("enter value:"))
+a=[]
+for i in range( 1,n+1):
+    a.append(i)
+print(a)
+
+sum=0
+for i in a:
+    sum=sum+i
+print(sum)
---
+++ cube of number.py
@@ -0,0 +1,3 @@
+n=int(input("enter the value:"))
+for i in range(1,n+1):
+    print("number is:",str(i),"and cube of number is:",i**3)
