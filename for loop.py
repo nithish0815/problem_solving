c6a51d6e143a321c19edf14518562424cff353cd
nithishkathirvel <kathirvelnithish08@gmail.com>
2025-01-27 20:36:24 +0530

    for loop practice

--- for loop.py
+++ for loop.py
@@ -1,4 +1,11 @@
-for i in range(1,11):
-    if(i%2==0):
-        print(i)
+n = int(input("enter :"))
+odd=0
+even=0
 
+for i in range(1,n+1):
+    if i%2 == 0 :
+        even+=1
+    else:
+        odd+=1
+print(even)
+print(odd)
