4bd7199049a1c1e69f753eb9445e827137ab2a25
nithishkathirvel <kathirvelnithish08@gmail.com>
2025-02-09 12:02:41 +0530

    python practice

---
+++ def fun.py
@@ -0,0 +1,8 @@
+def findpassorfail (num):
+    if num >= 35:
+        print("pass")
+    else:
+        print("fail")
+
+num=int(input("enter number:"))
+findpassorfail(num)
---
+++ fun input.py
@@ -0,0 +1,6 @@
+def printrange(a,b):
+    for i in range(a,b+1):
+        print(i)
+a=int(input("enter the value of a:"))
+b=int(input("enter the value of b:"))
+printrange(a,b)
