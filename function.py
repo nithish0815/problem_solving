cc4633b9f2778d872faf20c054f60865429d8367
nithishkathirvel <kathirvelnithish08@gmail.com>
2025-02-09 11:17:09 +0530

    python practice

---
+++ function.py
@@ -0,0 +1,15 @@
+def add():
+    print("Addition")
+    a=int(input("Enter a:"))
+    b=int(input("Enter b:"))
+    print(a+b)
+
+def sub():
+    print("Subtraction")
+    a=int(input("Enter a:"))
+    b=int(input("Enter b:"))
+    print(a-b)
+
+add()
+sub()
+
