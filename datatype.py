9428dc2fe3b4017b40904012add12b07ce464fd7
nithishkathirvel <kathirvelnithish08@gmail.com>
2025-04-17 15:21:16 +0530

    python practice

---
+++ datatype.py
@@ -0,0 +1,9 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+height = float(input("Enter your height in meters: "))
+
+print("\n--- User Details ---")
+print("Name:", name)
+print("Age:", age)
+print("Height:", height, "meters")
+
