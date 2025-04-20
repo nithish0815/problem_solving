cd4babe7843fdbc374f7e948109cd9a2416de171
nithishkathirvel <kathirvelnithish08@gmail.com>
2025-04-20 10:40:07 +0530

    python practice

--- classeg.py
+++ classeg.py
@@ -1,17 +1,19 @@
 class student:
-    def __init__(self):
-        self.name="name"
-        self.regno="108"
+    def __init__(self,name,reg):
+        self.name=name
+        self.regno=reg
     def display(self):
-        print("Name:",self.name)
-        print("Regno:",self.regno)
-s1=student()
-s2=student()
+        return (f"NAME:{self.name}\n"
+               f"REG.NO: {self.regno}")
 
-s1.name="Manoj"
-s1.regno="1"
-s2.name="Karthi"
-s2.regno="2"
-
-s1.display()
-s2.display()
+s1=student("nithish",12385)
+print(s1.display())
+# s2=student()
+#
+# s1.name="Manoj"
+# s1.regno="1"
+# s2.name="Karthi"
+# s2.regno="2"
+#
+# s1.display()
+# s2.display()
